cf2bd428879288fcc7c8547b16d951f76893ece3
krlmari <krylova_m_v@inbox.ru>
2021-11-15 21:45:14 +0300

    add signup and signin

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const { errors } = require("celebrate");
+
+const users = require("./routes/users");
+const movies = require("./routes/movies");
+
+const { createUser, login } = require("./controllers/users");
+
 const app = express();
+app.use(express.json());
 
 mongoose.connect("mongodb://localhost:27017/bitfilmsdb", {
   useNewUrlParser: true,
 });
 
-app.listen(3000);
+app.post("/signin", login);
+app.post("/signup", createUser);
+
+app.use("/", users);
+app.use("/", movies);
+
+app.use(errors);
+app.use(errorMain);
+
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send(err.message);
+    return;
+  }
+  res
+    .status(500)
+    .send({ message: `На сервере произошла ошибка: ${err.message}` });
+  next();
+});
+
+app.listen(PORT, () => {});
--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
@@ -25,7 +26,45 @@ const updateUser = (req, res, next) => {
     .catch(next);
 };
 
+const createUser = (req, res, next) => {
+  const { email, name } = req.body;
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) =>
+      User.create({
+        email,
+        name,
+        password: hash,
+      })
+    )
+    .then((user) => res.send({ data: user }))
+    .catch(() => {
+      throw new IncorrectError(
+        "Некорректные данные для создания нового пользователя."
+      );
+    })
+    .catch(next);
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, "some-secret-key", {
+        expiresIn: "7d",
+      });
+      res.send({ token });
+    })
+    .catch(() => {
+      throw new AuthError("Некорректные почта и/или пароль.");
+    })
+    .catch(next);
+};
+
 module.exports = {
   getUserId,
   updateUser,
+  createUser,
+  login,
 };
