368fba160583d97dd697be02f888de6ab2882510
krlmari <krylova_m_v@inbox.ru>
2021-11-15 20:03:57 +0300

    connect to mongoose

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/bitfilmsdb", {
+  useNewUrlParser: true,
+});
+
+app.listen(3000);
